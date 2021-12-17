bebbf7ba36a3892ef61b14f7026cd3cc74c72c71
Chad <chadlada@gmail.com>
2021-12-16 23:48:37 -0500

    Working-Done

--- Program.cs
+++ Program.cs
@@ -7,72 +7,31 @@ namespace AllCardsOnDeckCS
     {
         static void Main(string[] args)
         {
-            //   Algorithm B)
-            //       -make a list of the 4 suits and call this list 'suits'
-            var suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
-
-            //       -make a list of the 13 ranks and call this list 'ranks'
-            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
-
             //       -Make new list of strings named 'deck'
-            var deck = new List<string>();
+            var deck = new List<string>() { "Ace of hearts", "2 of hearts", "3 of hearts", "4 of hearts", "5 of hearts", "6 of hearts", "7 of hearts", "8 of hearts", "9 of hearts", "10 of hearts", "Jack of hearts", "Queen of hearts", "King of hearts", "Ace of diamonds", "2 of diamonds", "3 of diamonds", "4 of diamonds", "5 of diamonds", "6 of diamonds", "7 of diamonds", "8 of diamonds", "9 of diamonds", "10 of diamonds", "Jack of diamonds", "Queen of diamonds", "King of diamonds", "Ace of spades", "2 of spades", "3 of spades", "4 of spades", "5 of spades", "6 of spades", "7 of spades", "8 of spades", "9 of spades", "10 of spades", "Jack of spades", "Queen of spades", "King of spades", "Ace of clubs", "2 of clubs", "3 of clubs", "4 of clubs", "5 of clubs", "6 of clubs", "7 of clubs", "8 of clubs", "9 of clubs", "10 of clubs", "Jack of clubs", "Queen of clubs", "King of clubs" };
 
-            //       -Make a loop that goes though the list of 'suits'
-            foreach (var suit in suits)
+            var deckLength = deck.Count;
+
+
+            for (var rightIndex = deckLength - 1; rightIndex >= 1; rightIndex--)
             {
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
 
-                //       -make a loop that goes through the list 'ranks'
-                foreach (var rank in ranks)
-                {
-                    //         -for each rank, make a new string of the form $"{rank} of {suits}"
-                    var card = $"{rank} of ${suit}";
-
-                    //         -add that newly formed string to the end of the deck list
-                    deck.Add(card);
-                    //       -same as algorithm A
-
-                    // SHUFFLING CARDS
-
-                    // numberOfCards = length of our deck
-                    var numberOfCards = deck.Count;
-
-                    // for rightIndex from numberOfCards - 1 down to 1 do:
-                    var rightIndex = numberOfCards - 1;
-
-                    //   leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer")
-                    var randomNumberGenerator = new Random();
-                    var leftIndex = randomNumberGenerator.Next(rightIndex);
-                    //   Now swap the values at rightIndex and leftIndex by doing this:
-
-
-                    //     leftCard = the value from deck[leftIndex]
-                    var leftCard = deck[leftIndex];
-                    //     rightCard = the value from deck[rightIndex]
-                    var rightCard = deck[rightIndex];
-
-                    //     deck[rightIndex] = leftCard
-                    deck[rightIndex] = leftCard;
-                    //     deck[leftIndex] = rightCard
-                    deck[leftIndex] = rightCard;
-
-
-
-                }
+                var leftCard = deck[leftIndex];
+                var rightCard = deck[rightIndex];
+                deck[rightIndex] = leftCard;
+                deck[leftIndex] = rightCard;
             }
+            var firstCard = deck[0];
+            Console.WriteLine(firstCard);
+            var secondCard = deck[1];
+            Console.WriteLine(secondCard);
+
+
+
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
